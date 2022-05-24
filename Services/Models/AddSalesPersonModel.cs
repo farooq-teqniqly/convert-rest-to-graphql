c61dcba183ce8c72f05676b529827aa4d60e1b2f
Farooq Mahmud <farooq.mahmud@marel.com>
2022-05-24 13:34:38 -0700

    wip

--- Services/Models/AddSalesPersonModel.cs
+++ Services/Models/AddSalesPersonModel.cs
@@ -1,25 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Services.Models
+﻿namespace Services.Models
 {
+    using System;
+
     public class AddSalesPersonModel
     {
         public string FirstName { get; set; }
+
         public string? MiddleName { get; set; }
 
         public string LastName { get; set; }
 
         public DateTime HireDate { get; set; }
+
         public int TerritoryId { get; set; }
+
         public string Gender { get; set; }
+
         public string JobTitle { get; set; }
+
         public string MaritalStatus { get; set; }
+
         public string LoginId { get; set; }
+
         public string NationalIdNumber { get; set; }
+
         public DateTime BirthDate { get; set; }
     }
 }
